9b5af7c82d692c1511961548f8d0d1c3bc43f357
haiyang5210 <haiyang5210@gmail.com>
2014-11-23 20:07:20 +0800

    Signed-off-by: haiyang5210 <haiyang5210@gmail.com>

--- bin/server.js
+++ bin/server.js
@@ -180,7 +180,7 @@ http.createServer(function (req, res) {
         var mod_name;
 
         var form = new formidable.IncomingForm();
-        form.uploadDir = path.resolve(__dirname + '/dir');
+        form.uploadDir = './dir';
         form.parse(req, function (err, fields, files) {
             try {
                 pkgjson = JSON.parse(fields.package_json);
